2d86b465bce3736a48c895a13570b60483cebea7
Luciano Cordeiro <lacsousa@yahoo.com.br>
2020-08-30 18:59:18 -0300

    Adicionando o back-end e inicializando a exibição dele pelo httpClient

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  photosList = [
-    {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
-      description: 'Leão'
-    },
-    {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
-      description: 'Leoa'
-    }
-  ];
+  photosList : Object[] = [];
+
+  constructor(http : HttpClient){
+    // console.log(http);
+    http
+      .get<Object[]>('http://localhost:3000/flavio/photos')
+      .subscribe(photos => this.photosList = photos);
+  }
+
 }
--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { PhotosModule } from './photos/photos.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { PhotosModule } from './photos/photos.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PhotosModule
+    PhotosModule,
+    HttpClientModule
 
   ],
   providers: [],
