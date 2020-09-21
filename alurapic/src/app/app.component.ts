ae5cafa3b2fa3ac06fb4b0ee3687c21ae20cefd1
Luciano Cordeiro <lacsousa@yahoo.com.br>
2020-09-20 23:24:53 -0300

    [3.8] Inserindo interface e iniciando separação em serviço. Tratamento de erros

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -12,9 +12,14 @@ export class AppComponent {
 
   constructor(http : HttpClient){
     // console.log(http);
+
+    // Um observable é lazy. Ele só vai nos dados se alguém se inscrever nele pelo subscribe
     http
       .get<Object[]>('http://localhost:3000/flavio/photos')
-      .subscribe(photos => this.photosList = photos);
+      .subscribe(
+        photos => this.photosList = photos,
+        err => console.log(err)
+      );
   }
 
 }
