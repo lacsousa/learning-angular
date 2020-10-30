738b5d4553dedeadf03da0b113762bdcd0dc0c7a
Luciano Cordeiro <lacsousa@yahoo.com.br>
2020-10-29 22:47:32 -0300

    Implementando o resolver para carregar logo os dados

--- alurapic/src/app/app-routing.module.ts
+++ alurapic/src/app/app-routing.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
-import { NotFoundComponent} from './errors/not-found/not-found.component';
+import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 
 const routes: Routes = [
-    { path: 'user/:userName', component: PhotoListComponent },
-    { path: 'p/add', component: PhotoFormComponent },
-    { path: '**', component: NotFoundComponent },
+  { path: 'user/:userName',
+    component: PhotoListComponent,
+    resolve: {
+      photosListResolve: PhotoListResolver
+    }
+  },
+
+  { path: 'p/add',
+    component: PhotoFormComponent
+  },
+
+  { path: '**',
+    component: NotFoundComponent
+  },
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes)],
-  exports: [ RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -15,15 +15,19 @@ export class PhotoListComponent implements OnInit {
 
   ngOnInit(): void{
 
-    const nameUser = this.activatedRoute.snapshot.params.userName;
-    // Um observable é lazy. Ele só vai nos dados se alguém se inscrever nele pelo subscribe
-    // Observables vem do RxJs usado quando vc trabalha com chamadas assíncronas
-    this.photoService
-    .listFromUser(nameUser)
-    .subscribe(
-        photos => this.photosListComponent = photos,
-        err => console.log(err)
-      );
+
+    this.photosListComponent = this.activatedRoute.snapshot.data.photosListResolve; //nome da propriedade no routing
+
+
+    // const nameUser = this.activatedRoute.snapshot.params.userName;
+    // // Um observable é lazy. Ele só vai nos dados se alguém se inscrever nele pelo subscribe
+    // // Observables vem do RxJs usado quando vc trabalha com chamadas assíncronas
+    // this.photoService
+    // .listFromUser(nameUser)
+    // .subscribe(
+    //     photos => this.photosListComponent = photos,
+    //     err => console.log(err)
+    //   );
   }
 
    // Apenas para injeção de dependências
