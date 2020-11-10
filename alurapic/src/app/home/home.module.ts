809dafe6fc0f8519c07a97b284155e58a2b9455e
Luciano Antonio Cordeiro de Sousa <lacs@petrobras.com.br>
2020-11-10 11:45:01 -0300

    Início das validações na autenticação

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from "@angular/core";
 import { SignInComponent } from "./signin/signin.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from '@angular/common';
+
 
 @NgModule({
-  declarations: [ SignInComponent ]
+  declarations: [ SignInComponent ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ]
 })
 export class HomeModule{}
--- alurapic/src/app/home/signin/signin.component.ts
+++ alurapic/src/app/home/signin/signin.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './signin.component.html'
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit{
 
   // Componente de página pode omitir o selector
   // quem carregará esse arquivo será o sistema de módulos do Angular
+
+  loginForm : FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
 }
