79084036fabdfc30605429decd07e57d35db5229
Luciano Antonio Cordeiro de Sousa <lacs@petrobras.com.br>
2020-11-10 12:54:13 -0300

    Criação do componente de mesagens ap-vmessage

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from "@angular/core";
-import { SignInComponent } from "./signin/signin.component";
-import { ReactiveFormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
+import { SignInComponent } from './signin/signin.component';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
   declarations: [ SignInComponent ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    VMessageModule
   ]
 })
 export class HomeModule{}
