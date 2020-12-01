fd125ffe1d002279a3bb5083c34bffd45bdabcde
Luciano Antonio Cordeiro de Sousa <lacs@petrobras.com.br>
2020-11-30 22:45:11 -0300

    Interceptadores e envio de token

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from "@angular/core";
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
+import { HeaderComponent } from './header/header.component';
+import { RequestInterceptor } from './auth/request.interceptor';
+
+
 
 @NgModule({
   declarations: [ HeaderComponent ],
@@ -9,6 +13,13 @@ import { HeaderComponent } from "./header/header.component";
   imports: [
     CommonModule,
     RouterModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestInterceptor,
+      multi: true
+    }
   ]
 })
 export class CoreModule { }
