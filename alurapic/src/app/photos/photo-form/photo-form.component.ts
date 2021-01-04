43faf888a135297fe632b2aee058e235d71ff0d7
Luciano Cordeiro <lacsousa@gmail.com>
2021-01-04 07:57:23 -0300

    Início upload

--- alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -9,6 +9,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class PhotoFormComponent implements OnInit {
 
   photoForm : FormGroup;
+  file: File;
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -20,4 +21,14 @@ export class PhotoFormComponent implements OnInit {
     })
   }
 
+  upload() {
+    // const dados = this.photoForm.getRawValue();
+    // console.log(dados);
+    const description = this.photoForm.get('description').value;
+    const allowComments = this.photoForm.get('allowComments').value;
+    console.log(description);
+    console.log(allowComments);
+    console.log(this.file);
+
+  }
 }
